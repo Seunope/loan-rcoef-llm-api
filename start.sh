e0e7b6f87607184512c3dc8c05f20cd2304f5686
seunope <seunope2004@yahoo.co.uk>
2025-04-01 15:35:22 +0100

    set up requiremnets for deployment

--- start.sh
+++ start.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Install Conda dependencies from environment.yml
-conda env create -f environment.yml || conda env update -f environment.yml
+# Install dependencies using pip
+pip install -r requirements.txt
 
-# Activate the Conda environment
-source activate rcoef-api 
-
-# Start FastAPI server
+# Start FastAPI app
 uvicorn main:app --host 0.0.0.0 --port $PORT
